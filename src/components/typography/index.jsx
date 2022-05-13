1f492aead3b4fbcdf883ed9f40b0544568435dea
p1nta <crispworm@gmail.com>
2022-05-14 00:40:43 +0300

    chore: update typography component

--- src/components/typography/index.jsx
+++ src/components/typography/index.jsx
@@ -1,35 +1,53 @@
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
 import s from './index.module.sass';
 
-const Typography = (props) => {
-  const {
-    className = '',
-    color = 'black', // black | white | gray_1 | gray_3
-    size = 14, // 14 | 16 | 18 | 24 | 32 | 48
-    weight = 'normal', // normal | bold
-    component = 'p', // p | span | h1 | h2 | h3 | h4 | h5 | h6,
-    align = 'left', // left | center | right
-    children,
-    ...other
-  } = props;
-
-  const styles = [
-    s[`color_${color}`],
-    s[`size_${size}`],
-    s[`weight_${weight}`],
-    s[`align_${align}`],
-    className,
-  ];
-
-  const Component = component;
-
+const Typography = ({
+  className,
+  color,
+  size,
+  weight,
+  component:  Component,
+  align,
+  children,
+  ...other
+}) => {
   return (
     <Component
-      className={styles.join(' ')}
+      className={cn([
+        s[`color_${color}`],
+        s[`size_${size}`],
+        s[`weight_${weight}`],
+        s[`align_${align}`],
+        className,
+      ])}
       {...other}
     >
       {children}
     </Component>
   );
 };
+
+Typography.propTypes = {
+  className: PropTypes.string,
+  color: PropTypes.oneOf(['black', 'white', 'gray_1', 'gray_3', 'gray_3']),
+  size: PropTypes.oneOf(['14', '16', '18', '24', '32', '48']),
+  weight: PropTypes.oneOf(['normal', 'medium', 'bold']),
+  component: PropTypes.oneOf(['p', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  align: PropTypes.oneOf(['left', 'center', 'right']),
+  children: PropTypes.node,
+};
+
+Typography.defaultProps = {
+  className: '',
+  color: 'black',
+  size: '14',
+  weight: 'normal',
+  component: 'p',
+  align: 'left',
+  children: null,
+};
+
 
 export default Typography;
