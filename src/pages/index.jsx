4642cebc39f42cc945155719a3082f0b61d012c5
p1nta <crispworm@gmail.com>
2022-05-14 01:23:20 +0300

    chore: pretify code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,96 +8,77 @@ import opticsImg from '../../public/images/optics.png';
 import intl from '../intl'; // temp.
 import s from './index.module.sass';
 
-export default () => (
-  <div>
-    <section className={s.main}>
-      {intl.title}
-      <Button>
-        {intl.contribute_cta}
-      </Button>
-    </section>
-    <section className={s.money_reason_wrapper}>
-      <Typography
-        component="h2"
-        className={s.money_reason_header}
-        size="32"
-        color="gray_1"
-        weight="bold"
-        align="center"
-      >
-        {intl.subtitle_0}
-      </Typography>
-
-      <div className={s.money_reason_content_block}>
-        <div>
-          <div className={s.money_reason_content_item_image_wrapper}>
-            <div className={s.image_resizer} />
-            <div>
-              <Image
-                src={tacticImg}
-                alt={intl.tactic}
-              />
-            </div>
-          </div>
-          <Typography
-            align="center"
-            size="18"
-            weight="medium"
-            color="gray_1"
-            className={s.money_reason_content_item_text}
-          >
-            {intl.tactic}
-          </Typography>
-        </div>
-        <div>
-          <div className={s.money_reason_content_item_image_wrapper}>
-            <div className={s.image_resizer} />
-            <div>
-              <Image
-                src={thermImg}
-                alt={intl.therm}
-              />
-            </div>
-          </div>
-          <Typography
-            align="center"
-            size="18"
-            weight="medium"
-            color="gray_1"
-            className={s.money_reason_content_item_text}
-          >
-            {intl.therm}
-          </Typography>
-        </div>
-        <div>
-          <div className={s.money_reason_content_item_image_wrapper}>
-            <div className={s.image_resizer} />
-            <div>
-              <Image
-                src={opticsImg}
-                alt={intl.optics}
-              />
-            </div>
-          </div>
-          <Typography
-            align="center"
-            size="18"
-            weight="medium"
-            color="gray_1"
-            className={s.money_reason_content_item_text}
-          >
-            {intl.optics}
-          </Typography>
-        </div>
+const renderSub0Item = ({ src, text }) => (
+  <div key={text}>
+    <div className={s.sub_0_content_item_image_wrapper}>
+      <div className={s.sub_0_content_item_image_wrapper_resizer} />
+      <div className={s.sub_0_content_item_image}>
+        <Image
+          src={src}
+          alt={text}
+        />
       </div>
-    </section>
-    <section>{intl.subtitle_1}</section>
-    <section>
-      {intl.subtitle_2}
-      <Slider />
-      {intl.slider_sub}
-      <Socials />
-    </section>
-    <Contact />
+    </div>
+    <Typography
+      align="center"
+      size="18"
+      weight="medium"
+      color="gray_1"
+      className={s.sub_0_content_item_text}
+    >
+      {text}
+    </Typography>
   </div>
 );
+
+export default () => {
+  const sub0 = [
+    {
+      src: tacticImg,
+      text: intl.tactic,
+    },
+    {
+      src: thermImg,
+      text: intl.therm,
+    },
+    {
+      src: opticsImg,
+      text: intl.optics,
+    },
+  ]
+
+  return (
+    <div>
+      <section className={s.main}>
+        {intl.title}
+        <Button>
+          {intl.contribute_cta}
+        </Button>
+      </section>
+      <section className="container">
+        <Typography
+          component="h2"
+          className={s.sub_0_header}
+          size="32"
+          color="gray_1"
+          weight="bold"
+          align="center"
+        >
+          {intl.subtitle_0}
+        </Typography>
+
+        <div className={s.sub_0_content_block}>
+          {sub0.map(renderSub0Item)}
+        </div>
+      </section>
+      <section className={cn(s.sub_1, 'container')}>{intl.subtitle_1}</section>
+      <section className={cn(s.sub_2, 'container')}>
+        {intl.subtitle_2}
+        <Slider />
+        {intl.slider_sub}
+        <Socials />
+      </section>
+      <Contact />
+    </div>
+  );
+}
