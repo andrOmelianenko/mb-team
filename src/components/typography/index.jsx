852934b9adafce9e1a62ce135439ebd300a1ebca
p1nta <crispworm@gmail.com>
2022-05-13 23:23:45 +0300

    chore: added typography element. start money_reason section

--- src/components/typography/index.jsx
+++ src/components/typography/index.jsx
@@ -1,6 +1,35 @@
-const Typography = () => {
-  // ..
-  return <div>Typography</div>;
+import s from './index.module.sass';
+
+const Typography = (props) => {
+  const {
+    className = '',
+    color = 'black', // black | white | gray_1 | gray_3
+    size = 14, // 14 | 16 | 18 | 24 | 32 | 48
+    weight = 'normal', // normal | bold
+    component = 'p', // p | span | h1 | h2 | h3 | h4 | h5 | h6,
+    align = 'left', // left | center | right
+    children,
+    ...other
+  } = props;
+
+  const styles = [
+    s[`color_${color}`],
+    s[`size_${size}`],
+    s[`weight_${weight}`],
+    s[`align_${align}`],
+    className,
+  ];
+
+  const Component = component;
+
+  return (
+    <Component
+      className={styles.join(' ')}
+      {...other}
+    >
+      {children}
+    </Component>
+  );
 };
 
 export default Typography;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { Contact, Slider, Socials, Button } from '../components';
+import { Contact, Slider, Socials, Button, Typography } from '../components';
 import intl from '../intl'; // temp.
 import s from './index.module.sass';
 
@@ -6,9 +6,49 @@ export default () => (
   <div>
     <section className={s.main}>
       {intl.title}
-      <Button>{intl.contribute_cta}</Button>
+      <Button>
+        {intl.contribute_cta}
+      </Button>
     </section>
-    <section>{intl.subtitle_0}</section>
+    <section className={s.money_reason_wrapper}>
+      <Typography
+        component="h2"
+        className={s.money_reason_header}
+        size="32"
+        color="gray_1"
+        weight="bold"
+        align="center"
+      >
+        {intl.subtitle_0}
+      </Typography>
+
+      <div className={s.money_reason_content_block}>
+        <div className={s.money_reason_content_item}>
+          <div className={s.money_reason_content_item_image_wrapper}>
+            <img src="" className={s.money_reason_content_item_image} />
+          </div>
+          <p className={s.money_reason_content_item_text}>
+            {intl.subtitle_0}
+          </p>
+        </div>
+        <div className={s.money_reason_content_item}>
+          <div className={s.money_reason_content_item_image_wrapper}>
+            <img src="" className={s.money_reason_content_item_image} />
+          </div>
+          <p className={s.money_reason_content_item_text}>
+            {intl.subtitle_0}
+          </p>
+        </div>
+        <div className={s.money_reason_content_item}>
+          <div className={s.money_reason_content_item_image_wrapper}>
+            <img src="" className={s.money_reason_content_item_image} />
+          </div>
+          <p className={s.money_reason_content_item_text}>
+            {intl.subtitle_0}
+          </p>
+        </div>
+      </div>
+    </section>
     <section>{intl.subtitle_1}</section>
     <section>
       {intl.subtitle_2}
