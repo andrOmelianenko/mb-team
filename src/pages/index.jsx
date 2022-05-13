b43dfe27f0d7eeed762ee2f168bc1e0893ca3f26
andrOmelianenko <o.andrriy@gmail.com>
2022-05-13 06:06:43 +0300

    feat: using next Image, containers for main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import cn from 'classnames';
 import { Contact, Slider, Socials, Button } from '../components';
 import intl from '../intl'; // temp.
 import s from './index.module.sass';
@@ -8,9 +10,20 @@ export default () => (
       {intl.title}
       <Button>{intl.contribute_cta}</Button>
     </section>
-    <section>{intl.subtitle_0}</section>
-    <section>{intl.subtitle_1}</section>
-    <section>
+    <section className={cn(s.sub_0, 'container')}>
+      {intl.subtitle_0}
+      <div>
+        <Image src="/images/tactic.png" width={133} height={139} alt="tactic" />
+      </div>
+      <div>
+        <Image src="/images/therm.png" width={117} height={105} alt="therm" />
+      </div>
+      <div>
+        <Image src="/images/optics.png" width={151} height={91} alt="optics" />
+      </div>
+    </section>
+    <section className={cn(s.sub_1, 'container')}>{intl.subtitle_1}</section>
+    <section className={cn(s.sub_2, 'container')}>
       {intl.subtitle_2}
       <Slider />
       {intl.slider_sub}
