f0d8da5781e864c9ea789c7328a71241d13443f2
andrOmelianenko <o.andrriy@gmail.com>
2022-07-19 05:55:27 +0300

    tech: updated next config due to global css importing inside infinite-react-carousel module

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 const { join } = require('path');
+const { withGlobalCss } = require('next-global-css');
 
-module.exports = {
+// temp. workaround 
+// (https://github.com/vercel/next.js/discussions/27953#discussioncomment-2186310)
+const withConfig = withGlobalCss();
+
+module.exports = withConfig({
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   sassOptions: {
     includePaths: [join(__dirname, 'src/styles')],
   },
-};
+
+});
