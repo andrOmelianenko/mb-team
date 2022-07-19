3981007ad2270d834656720098a19eedc9f864de
andrOmelianenko <o.andrriy@gmail.com>
2022-07-19 05:56:52 +0300

    feat: infinite carousel implemented

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,6 +1,36 @@
-const Slider = () => {
-  // ..
-  return <div>Slider</div>;
-};
+import Image from 'next/image';
+import Slider from 'infinite-react-carousel';
+import slideImg from '../../../public/images/slide.jpg';
+import { Arrow } from '../../assets/svg';
+import s from './index.module.sass';
 
-export default Slider;
+export default () => {
+  const renderArrow = (side) => {
+    return (
+      <div>
+        <Arrow className={s[side]} />
+      </div>
+    );
+  };
+
+  // temp. for example
+  const renderSlides = () =>
+    Array.apply(null, Array(9)).map((e, i) => (
+      <div key={i} className={s.slide}>
+        <Image src={slideImg} alt="" />
+      </div>
+    ));
+
+  return (
+    <Slider
+      overScan={5}
+      slidesPerRow={3}
+      virtualList
+      className={s.slider}
+      prevArrow={renderArrow('prev')}
+      nextArrow={renderArrow('next')}
+    >
+      {renderSlides()}
+    </Slider>
+  );
+};
